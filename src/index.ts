65c7869ef6084a993568775aba01310012471f7d
Grant Kiely <grant.kiely@gmail.com>
2022-09-02 16:58:45 -0500

    Added files

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import {
+  ESLintUtils,
+  AST_NODE_TYPES,
+} from '@typescript-eslint/experimental-utils';
+import * as ts from 'typescript';
+import * as tsutils from 'tsutils';
+
+const createRule = ESLintUtils.RuleCreator(name => name);
+
+export default createRule<[], 'jsxNumber&&'>({
+  name: 'jsx-no-leaked-render',
+  defaultOptions: [],
+  meta: {
+    type: 'problem',
+    docs: {
+      description:
+        'Prevent boolean expressions from adding unwanted falsy numbers to your JSX',
+      category: 'Possible Errors',
+      recommended: 'error',
+      requiresTypeChecking: true,
+    },
+    messages: {
+      'jsxNumber&&': 'This expression could evaluate to 0 or NaN.',
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      LogicalExpression: expr => {
+        if (expr.operator !== '&&') return;
+        if (
+          !expr.parent ||
+          expr.parent.type !== AST_NODE_TYPES.JSXExpressionContainer
+        ) {
+          return;
+        }
+        const service = context.parserServices;
+        const checker = service?.program?.getTypeChecker();
+        const tsNode = service?.esTreeNodeToTSNodeMap?.get(expr.left);
+        if (!checker || !tsNode) {
+          throw new Error('Parser service not available!');
+        }
+        const leftNodeType = checker.getTypeAtLocation(tsNode);
+        const constrainedType = checker.getBaseConstraintOfType(leftNodeType);
+        const type = constrainedType ?? leftNodeType;
+
+        let isLeftNodeNumber = tsutils.isTypeFlagSet(
+          type,
+          ts.TypeFlags.NumberLike
+        );
+
+        let isLeftNodeAny = tsutils.isTypeFlagSet(type, ts.TypeFlags.Any);
+
+        if (!isLeftNodeNumber && !isLeftNodeAny && tsutils.isUnionType(type)) {
+          for (const ty of type.types) {
+            if (tsutils.isTypeFlagSet(ty, ts.TypeFlags.NumberLike)) {
+              isLeftNodeNumber = true;
+              break;
+            }
+            if (tsutils.isTypeFlagSet(ty, ts.TypeFlags.Any)) {
+              isLeftNodeAny = true;
+              break;
+            }
+          }
+        }
+
+        if (isLeftNodeNumber || isLeftNodeAny) {
+          context.report({
+            node: expr.left,
+            messageId: 'jsxNumber&&',
+          });
+        }
+      },
+    };
+  },
+});
---
+++ tests/index.test.ts
@@ -0,0 +1,60 @@
+import { TSESLint } from '@typescript-eslint/experimental-utils';
+import rule from '../src';
+
+const parserOptions: TSESLint.ParserOptions = {
+  sourceType: 'module',
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions,
+});
+
+ruleTester.run('jsx-no-leaked-render', rule, {
+  valid: [
+    {
+      code: `<>{true ? <></> : ''}</>`,
+    },
+    {
+      code: `<>{true && <></>}</>`,
+    },
+    {
+      code: `<>{undefined && <></>}</>`,
+    },
+    {
+      code: `<>{'' && <></>}</>`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+        const t = 0;
+        <>{t && <></>}</>
+      `,
+      errors: [
+        {
+          messageId: 'jsxNumber&&',
+        },
+      ],
+    },
+    {
+      code: `<>{0 && <></>}</>`,
+      errors: [
+        {
+          messageId: 'jsxNumber&&',
+        },
+      ],
+    },
+    {
+      code: ` const t = NaN; <>{t && <></>}</>`,
+      errors: [
+        {
+          messageId: 'jsxNumber&&',
+        },
+      ],
+    },
+  ],
+});
